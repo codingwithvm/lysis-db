93e2e28026e80b8e7a963b8c1d62c5c59deceeef
João Victor Morais Mendes <joao.mendes.valenca@gmail.com>
2025-11-25 16:28:29 -0300

    fix: add missing space in gunicorn command for consistency

--- prod.py
+++ prod.py
@@ -8,7 +8,7 @@ if sys.platform.startswith("win"):
   cmd = "uvicorn src.main:app --host 0.0.0.0 --port 4545"
 else:
   cmd = (
-    "gunicorn -k uvicorn.workers.UvicornWorker"
+    "gunicorn -k uvicorn.workers.UvicornWorker "
     "src.main:app --bind 0.0.0.0:4545 --workers 4"
   )
 
